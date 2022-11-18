4dc31398c65d7cc0d8a9835186954ba7e000260d
Oscar Ortiz <fabioc.fo64@gmail.com>
2022-11-17 18:58:59 -0600

    Project Setup

---
+++ src/main/java/com/paredifa/paredifa_backend/ParedifaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.paredifa.paredifa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParedifaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParedifaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paredifa/paredifa_backend/ParedifaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paredifa.paredifa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParedifaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
