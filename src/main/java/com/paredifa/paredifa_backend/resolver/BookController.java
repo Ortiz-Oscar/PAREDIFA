de995b9fba0b076461148941863826ac68515b68
Oscar Ortiz <fabioc.fo64@gmail.com>
2022-11-20 12:53:18 -0600

    Simple Project Setup

---
+++ src/main/java/com/paredifa/paredifa_backend/resolver/BookController.java
@@ -0,0 +1,21 @@
+package com.paredifa.paredifa_backend.resolver;
+
+import com.paredifa.paredifa_backend.resolver.automata.Book;
+import com.paredifa.paredifa_backend.resolver.regex.Author;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController {
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.getAuthorId());
+    }
+}
